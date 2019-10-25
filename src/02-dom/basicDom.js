907e6c90809f07883084caf3125aaacb256c7258
asif <asif@ifms-MacBook-Air.local>
2019-10-25 08:57:19 -0600

    starting version of DOM ex.1 (Comp 110)

---
+++ src/02-dom/basicDom.js
@@ -0,0 +1,14 @@
+
+let divCont = document.getElementsByClassName("container");
+let divManipulate = divCont.addEventListener("click", () => {
+  console.log("click");
+});
+
+
+// document.getElementsByClassName("container").addEventListener("click", () => {
+//   console.log("click");
+// });
+
+
+
+// export default domFunc;
---
+++ src/02-dom/mainDom.js
@@ -0,0 +1,10 @@
+// import functions from './basicDom.js';
+
+// let divCont = document.getElementsByClassName("container");
+// let divManipulate = divCont.addEventListener("click", () => {
+//   console.log("click");
+// });
+
+
+
+
